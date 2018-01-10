f70443d77105743b31c560de7b5a7ba0e070b755
Nathan De Priest <nathan.depriest@gmail.com>
2018-01-09 19:41:17 -0600

    changed variables from global scope to block scope.
    sends you a message for the event you signed up for, the activities the event involves, and the days you have left to train.

---
+++ Functions/training-days.js
@@ -0,0 +1,46 @@
+const getAllEvents = () => {
+  return ['Marathon', 'Triathlon', 'Decathlon'];
+}; 
+
+const getRandomEvent = () => {
+  const allEvents = getAllEvents();
+  const event = allEvents[Math.floor(Math.random() * allEvents.length)];
+  return event;
+};
+
+
+const getEventActivities = event => {
+  const allEvents = getAllEvents();
+  if (!allEvents.includes(event)) {
+    return null;
+  }
+
+  let activities;
+  if (event === 'Marathon') {
+    activities = ['Running'];
+  }
+  if (event === 'Triathlon') {
+    activities = ['Running', 'Cycling', 'Swimming'];
+  }
+  if (event === 'Decathlon') {
+    activities = ['Running', 'Hurdles', 'Javelin throw', 'Discus Throw', 'Shot put', 'High Jump'];
+  }
+  return activities.join(', ');
+};
+
+const getDaysToTrain = event => {
+  const allEvents = getAllEvents();
+  if (!allEvents.includes(event)) {
+    return null;
+ }
+
+  const eventTrainingTimes = {'Marathon': 50, 'Triathlon': 100, 'Decathlon': 200 };
+  return eventTrainingTimes[event];
+};
+
+const getEventMessage = () => {
+  const myEvent = getRandomEvent();
+  console.log('Your event is a ' + myEvent + '. Your event activities consist of ' + getEventActivities(myEvent) + '. You have ' + getDaysToTrain(myEvent) +  ' days to train.');
+};
+
+getEventMessage();
